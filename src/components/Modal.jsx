6a7b652076338caa9804a51918a6230baca8a908
SonJHo <wnsgh708222@naver.com>
2025-03-11 13:59:29 +0900

    모달창 업뎃
    기능은 아직

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,26 +1,42 @@
-import "./Modal.css"
+import "./Modal.css";
+import { useState } from "react";
 
 const Modal = ({ openModal, setOpenModal }) => {
-    return(
+    return (
         <div className="Modal">
-        <div className="Overlay">
-            <div className="container">
-                <button
-                    className="cancle"
-                    type="button"
-                    onClick={() => {
-                        setOpenModal(false); // 클릭 이벤트로 모달창 닫히게 하기
-                    }}
-                >
-                    취소
-                </button>
+            <div className="Overlay">
+                <div className="container">
+                    <section>
+                        <div className="search-box">
+                            <input
+                                type="text"
+                                className="search-input"
+                                placeholder="email로 검색하세요요"
+                            />
+                            <button className="search-button">검색</button>
+                        </div>
+                        
+                        <div className="search-results">
+                            <p>검색 결과가 여기에 표시됩니다.</p>
+                        </div>
+                    </section>
 
-                친구검색기능 추가해야함
-                {!openModal ? setOpenModal(true) : null}
+                    <button
+                        className="cancel"
+                        type="button"
+                        onClick={() => {
+                            setOpenModal(false); // 클릭 이벤트로 모달창 닫히게 하기
+                        }}
+                    >
+                        닫기
+                    </button>
+
+                   
+                    {!openModal ? setOpenModal(true) : null}
+                </div>
             </div>
         </div>
-    </div>
-    )
-}
+    );
+};
 
-export default Modal
+export default Modal;
