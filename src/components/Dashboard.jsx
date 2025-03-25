a2757e887713867a6d53c0897fd4bc12590bf4ff
SonJHo <wnsgh708222@naver.com>
2025-03-25 16:49:48 +0900

    첫 병합완료
    완

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -7,26 +7,23 @@ const Dashboard = () => {
         <div className="Dashboard">
             <div className="Content-container">
 
-                <div className="main-container">메인컨테이너
+                <div className="main-container">
                     <section className="board-area">
-                    영역1
-                    <NormalBoard/>
+                        <NormalBoard />
                     </section>
 
                     <section className="board-area">
-                    영역2
+                        영역2
 
-                    <div>자유게시판</div>
-                    <div>강의게시판판</div>
+                        <div>자유게시판</div>
+                        <div>강의게시판판</div>
+                        <div>강의게시판판</div>
 
                     </section>
 
 
                     <section className="board-area">
-                    영역3
-
-                    <div>인기게시판판</div>
-                    <div>익명게시판판</div>
+                        <BookMarketBoard/>
 
                     </section>
 
--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -2,13 +2,7 @@ import "./Panel.css";
 import Favorites from "./Favorites";
 import Friends from "./Friends";
 import { Link , useNavigate} from "react-router-dom";
-import axiosInstance from "./utils/AxiosInstance";
-import handleApiError from "./utils/handleApiError";
 import Calendar from "./Calendar";
-
-
-
-import { Link } from "react-router-dom";
 const Panel = () => {
     const name = localStorage.getItem("name")
     return (
