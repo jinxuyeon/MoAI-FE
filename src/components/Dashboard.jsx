0876ed7712079f9147576f7c59878bcea452848c
SonJHo <wnsgh708222@naver.com>
2025-03-25 16:21:24 +0900

    대시보드 레이어 수동병함
    ㄴㄴㅇㄹ

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,9 +6,33 @@ const Dashboard = () => {
     return (
         <div className="Dashboard">
             <div className="Content-container">
-                <NormalBoard/>
-                <BookMarketBoard/>
-                
+
+                <div className="main-container">메인컨테이너
+                    <section className="board-area">
+                    영역1
+                    <NormalBoard/>
+                    </section>
+
+                    <section className="board-area">
+                    영역2
+
+                    <div>자유게시판</div>
+                    <div>강의게시판판</div>
+
+                    </section>
+
+
+                    <section className="board-area">
+                    영역3
+
+                    <div>인기게시판판</div>
+                    <div>익명게시판판</div>
+
+                    </section>
+
+                </div>
+                <div className="rightside-container">취업정보컨테이너너</div>
+
             </div>
         </div>
     )
