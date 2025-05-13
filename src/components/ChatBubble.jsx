47640a084ed426cb736573c7ad02b8426d3f523e
SonJHo <wnsgh708222@naver.com>
2025-05-13 15:44:23 +0900

    레이아웃 디자인개선선

---
+++ src/components/ChatBubble.jsx
@@ -0,0 +1,13 @@
+import "./ChatBubble.css";
+
+const ChatBubble = () => {
+    return (
+        <div className="ChatBubble">
+            <div className="content-box">
+                <p className="content">대화내sdfdsfsdfsdfsdfsdfsdfsdfsd용 예시.</p>
+            </div>
+        </div>
+    );
+};
+
+export default ChatBubble;
