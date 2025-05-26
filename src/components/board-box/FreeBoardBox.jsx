303d88ad24079371975b56226f440910e3bdf0d5
choiyoonjoo <145316737+choiyoonjoo@users.noreply.github.com>
2025-05-26 22:05:09 +0900

    ㅇㅇ

--- src/components/board-box/FreeBoardBox.jsx
+++ src/components/board-box/FreeBoardBox.jsx
@@ -4,16 +4,16 @@ import "./FreeBoardBox.css";
 
 const FreeBoardBox = () => {
   const dummyFrees = [
-    { id: 15, title: "오늘 날씨 진짜 좋네요 ☀️", author: "서예빈", date: "2025-05-01", views: 12, imgUrl:" https://mblogthumb-phinf.pstatic.net/MjAyMjAyMDdfMjEy/MDAxNjQ0MTk0Mzk2MzY3.WAeeVCu2V3vqEz_98aWMOjK2RUKI_yHYbuZxrokf-0Ug.sV3LNWlROCJTkeS14PMu2UBl5zTkwK70aKX8B1w2oKQg.JPEG.41minit/1643900851960.jpg?type=w800" },
-    { id: 14, title: "프론트엔드 공부 어떻게 시작했어요?", author: "익명", date: "2025-04-30", views: 55 },
-    { id: 13, title: "컴공 시험 범위 아시는 분?", author: "최윤주", date: "2025-04-29", views: 45, imgUrl: "https://mblogthumb-phinf.pstatic.net/MjAyMTEyMTJfMjg2/MDAxNjM5MjQ5ODI1NTU3.bEgs_j_ZaQgWAgAnjJ2daUIQlUzKuAud4l6KRXgqiw0g.qt0LjkvDb9WRPR1j_WEOjwZS642aXaI36iWDxvsqW2Ug.JPEG.41minit/1639153272211%EF%BC%8D4.jpg?type=w800" },
-    { id: 12, title: "자취 꿀템 추천좀 해주세요", author: "홍길동", date: "2025-04-28", views: 25 },
-    { id: 11, title: "친해져요!", author: "익명", date: "2025-04-27", views: 18 },
-    { id: 10, title: "헬스장 같이 다니실 분 구해요", author: "익명", date: "2025-04-26", views: 50, imgUrl: "https://mblogthumb-phinf.pstatic.net/MjAyMTEyMTJfODcg/MDAxNjM5MjQ5ODI4MDUz.RK_Ci32uWkgzv4sTTrlLowPU0bLil6qVMF1jTLKJHEAg.ra83ge0ifH7-tyv7mwa-3WOJL8sgOcSk3qQ80YUMvC0g.JPEG.41minit/1639153272211%EF%BC%8D7.jpg?type=w800" },
-    { id: 9, title: "상담 후기 ㅠ", author: "김민수", date: "2025-04-25", views: 27 },
-    { id: 8, title: "다들 꿈이 뭐양?", author: "이민지", date: "2025-04-23", views: 41, imgUrl: "https://mblogthumb-phinf.pstatic.net/MjAyMTEyMjJfMTA5/MDAxNjQwMTUwOTUwNTE3.X_5x1TPHLfj7aQpP7xxk3o8M85B8AhUFuB7C7Z8WFPEg.7fAWNV1H5ITpZVUPuYG9o38x6shm3llnDAO_Xtqs-U4g.JPEG.41minit/1640148412116.jpg?type=w800" },
-    { id: 7, title: "반팔 언제 입을꺼야??", author: "익명", date: "2025-04-22", views: 38 },
-    { id: 6, title: "친구 문제 해결 좀 ㅜ", author: "익명", date: "2025-04-21", views: 22 }
+    { id: 15, title: "오늘 날씨 진짜 좋네요 ☀️", author: "서예빈", date: "2025-05-01", views: 12, like: 25, ment: 2, imgUrl: "https://mblogthumb-phinf.pstatic.net/MjAyMjAyMDdfMjEy/MDAxNjQ0MTk0Mzk2MzY3.WAeeVCu2V3vqEz_98aWMOjK2RUKI_yHYbuZxrokf-0Ug.sV3LNWlROCJTkeS14PMu2UBl5zTkwK70aKX8B1w2oKQg.JPEG.41minit/1643900851960.jpg?type=w800" },
+    { id: 14, title: "프론트엔드 공부 어떻게 시작했어요?", author: "익명", date: "2025-04-30", views: 55, like: 25, ment: 2 },
+    { id: 13, title: "캡공 시험 범위 아시는 분?", author: "최윤주", date: "2025-04-29", views: 45, like: 25, ment: 2, imgUrl: "https://mblogthumb-phinf.pstatic.net/MjAyMTEyMTJfMjg2/MDAxNjM5MjQ5ODI1NTU3.bEgs_j_ZaQgWAgAnjJ2daUIQlUzKuAud4l6KRXgqiw0g.qt0LjkvDb9WRPR1j_WEOjwZS642aXaI36iWDxvsqW2Ug.JPEG.41minit/1639153272211%EF%BC%8D4.jpg?type=w800" },
+    { id: 12, title: "자취템 추천좀 해주세요", author: "홍길동", date: "2025-04-28", views: 25, like: 25, ment: 2 },
+    { id: 11, title: "친해져요!", author: "익명", date: "2025-04-27", views: 18, like: 25, ment: 2 },
+    { id: 10, title: "헬스장 같이 다니실 분 구해요", author: "익명", date: "2025-04-26", views: 50, like: 25, ment: 2, imgUrl: "https://mblogthumb-phinf.pstatic.net/MjAyMTEyMTJfODcg/MDAxNjM5MjQ5ODI4MDUz.RK_Ci32uWkgzv4sTTrlLowPU0bLil6qVMF1jTLKJHEAg.ra83ge0ifH7-tyv7mwa-3WOJL8sgOcSk3qQ80YUMvC0g.JPEG.41minit/1639153272211%EF%BC%8D7.jpg?type=w800" },
+    { id: 9, title: "상담 후기", author: "김민수", date: "2025-04-25", views: 27, like: 25, ment: 2 },
+    { id: 8, title: "다들 꿈이 뭐야?", author: "이민지", date: "2025-04-23", views: 41, like: 25, ment: 2, imgUrl: "https://mblogthumb-phinf.pstatic.net/MjAyMTEyMjJfMTA5/MDAxNjQwMTUwOTUwNTE3.X_5x1TPHLfj7aQpP7xxk3o8M85B8AhUFuB7C7Z8WFPEg.7fAWNV1H5ITpZVUPuYG9o38x6shm3llnDAO_Xtqs-U4g.JPEG.41minit/1640148412116.jpg?type=w800" },
+    { id: 7, title: "반팔 언제 입을꺼야??", author: "익명", date: "2025-04-22", views: 38, like: 25, ment: 2 },
+    { id: 6, title: "친구 문제 해결 좀 ", author: "익명", date: "2025-04-21", views: 22, like: 25, ment: 2 }
   ];
 
   const [currentPage, setCurrentPage] = useState(1);
@@ -34,29 +34,31 @@ const FreeBoardBox = () => {
   return (
     <div className="FreeBoardBox">
       <div className="free-header">
-        <h2 className="free-title">자유게시판</h2>
-        {/* <Link to="/main/free/write" className="write-button">글쓰기</Link> */}
+        <h2 className="Free-title">자유게시판</h2>
       </div>
 
       <div className="free-list">
         {visibleFrees.map((free) => (
           <div key={free.id} className="free-list-item">
             {free.imgUrl && (
-              <img
-                src={free.imgUrl}
-                alt="썸네일"
-                className="free-thumbnail"
-              />
+              <img src={free.imgUrl} alt="썸네일" className="free-thumbnail" />
             )}
             <div className="free-list-content">
-              <Link to={`/main/free/${free.id}`} className="free-title-link">
-                {free.title}
-                {free.views >= 40 && (
-                  <span className="hot-badge">인기</span>
-                )}
+              <Link to={`/main/free/${free.id}`} className="free-link">
+                <div className="free-title-line">
+                  <div className="free-title-wrapper">
+                    <h3 className="free-title">{free.title}</h3>
+                    {free.views >= 40 && (
+                      <span className="hot-badge">인기</span>
+                    )}
+                  </div>
+                  <div className="free-author-date">
+                    {free.author} | {free.date}
+                  </div>
+                </div>
               </Link>
-              <div className="free-meta">
-                {free.author} | {free.date} | 조회 {free.views}
+              <div className="free-meta-line">
+                조회 {free.views} | ❤️ {free.like} | 댓글 {free.ment}
               </div>
             </div>
           </div>
@@ -65,9 +67,7 @@ const FreeBoardBox = () => {
 
       <div className="pagination">
         {currentPage > 1 && (
-          <button onClick={() => handlePageChange(currentPage - 1)}>
-            &lt; 이전
-          </button>
+          <button onClick={() => handlePageChange(currentPage - 1)}>&lt; 이전</button>
         )}
         {Array.from({ length: totalPages }, (_, index) => (
           <button
@@ -79,9 +79,7 @@ const FreeBoardBox = () => {
           </button>
         ))}
         {currentPage < totalPages && (
-          <button onClick={() => handlePageChange(currentPage + 1)}>
-            다음 &gt;
-          </button>
+          <button onClick={() => handlePageChange(currentPage + 1)}>다음 &gt;</button>
         )}
       </div>
     </div>
--- src/components/board-box/SecretBoardBox.jsx
+++ src/components/board-box/SecretBoardBox.jsx
@@ -4,23 +4,22 @@ import "./SecretBoardBox.css";
 
 const SecretBoardBox = () => {
     const dummySecrets = [
-        { id: 15, title: "혼밥 너무 외로워요", author: "익명", date: "2025-05-15", views: 16, imgUrl: "https://i.namu.wiki/i/9aUQQ4YjU9vmKuHT_cZAL61VKpKsLolynnI46BhOZQuKxGJygZ6BJK2zTHoX3pcNQmmcfzcVEZQcythY1lRXBQ.webp" },
-        { id: 14, title: "좋아하는 사람 생겼는데 말 못해요", author: "익명", date: "2025-05-14", views: 34 },
-        { id: 13, title: "부모님께 진로 거짓말했어요", author: "익명", date: "2025-05-13", views: 20 },
-        { id: 12, title: "공허한 느낌 벗어나고 싶어요", author: "익명", date: "2025-05-12", views: 45, imgUrl: "https://i.namu.wiki/i/Va3Dy_3qFHvGQS4qwv0oCvFySbT1DXJkK0zfMosd2UK6Jun8Zucb796VLJzLL4A40e5P4dgbBPT4da2Bv_S50Q.webp" },
-        { id: 11, title: "친구한테 질투하는 내가 싫어요", author: "익명", date: "2025-05-11", views: 30 },
-        { id: 10, title: "누군가 계속 쳐다보는 것 같아요", author: "익명", date: "2025-05-10", views: 26, imgUrl: "https://mblogthumb-phinf.pstatic.net/MjAyMjExMTZfMTQz/MDAxNjY4NjAxNTQzNDYw.dZzc1TIxWmITM_5uildryLFGXFwgjx0ahbrf9DXCEZ0g.xTwwfY8Je-4zuVM3FbIm2WDtEY0b9YSimgX4RM6MCsEg.JPEG.gngnt2002/%EF%BB%BF%EC%9A%B0%EB%8A%94_%EA%B3%A0%EC%96%91%EC%9D%B4_%EC%A7%A4_%EB%AA%A8%EC%9D%8C_(4).jpg?type=w800" },
-        { id: 9, title: "그냥 사라지고 싶다는 생각이 자주 들어요", author: "익명", date: "2025-05-09", views: 47 },
-        { id: 8, title: "미래가 너무 막막해서 무서워요", author: "익명", date: "2025-05-08", views: 33, imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIJ2B3kX3bAh4NYcgHmuDVHDjt3AxpQJjvKw&s" },
-        { id: 7, title: "누군가에게 의미 있는 사람이 되고 싶어요", author: "익명", date: "2025-05-07", views: 28 },
-        { id: 6, title: "SNS 속 나는 가짜 같아요", author: "익명", date: "2025-05-06", views: 39 },
-        { id: 5, title: "자존감이 점점 낮아져요", author: "익명", date: "2025-05-05", views: 32, imgUrl: "https://mblogthumb-phinf.pstatic.net/MjAyMjExMTZfMjYx/MDAxNjY4NjAxNTQzODcz.tRQa3wRkWURAgSonPag7uhSW36336w6o4zKdvQiFOFUg.Mf2SuR42X0IOV8biEh-tFgC_6YE8reIF8YDohv5Ar3Mg.JPEG.gngnt2002/%EF%BB%BF%EC%9A%B0%EB%8A%94_%EA%B3%A0%EC%96%91%EC%9D%B4_%EC%A7%A4_%EB%AA%A8%EC%9D%8C_(8).jpg?type=w800" },
-        { id: 4, title: "밤마다 걱정이 많아져서 잠이 안 와요", author: "익명", date: "2025-05-04", views: 36 },
-        { id: 3, title: "무리 속에서도 혼자인 느낌 들어요", author: "익명", date: "2025-05-03", views: 19 },
-        { id: 2, title: "나도 남들처럼 살고 싶어요", author: "익명", date: "2025-05-02", views: 25 },
-        { id: 1, title: "익명이라 말해요, 저 지금 우울해요", author: "익명", date: "2025-05-01", views: 51, imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPqC-w86RQ5lEBg29R9dBdxYdzmdqVULlKMw&s" }
+        { id: 15, title: "혼밥 너무 외로워요", author: "익명", date: "2025-05-15", views: 16,like: 25, ment: 2, imgUrl: "https://i.namu.wiki/i/7SO2FDuNnzmK_kE68K_wceSKJqoW8-E4vQnJE3uAItSdqFbjbwHMgITRfWLnssiT7MLWzTz3n6nBedGTFFC1EA.webp" },
+        { id: 14, title: "좋아하는 사람 생겼는데 말 못해요", author: "익명", date: "2025-05-14", views: 34 ,like: 25, ment: 2},
+        { id: 13, title: "부모님께 진로 거짓말했어요", author: "익명", date: "2025-05-13", views: 20 ,like: 25, ment: 2},
+        { id: 12, title: "공허한 느낌 벗어나고 싶어요", author: "익명", date: "2025-05-12", views: 45, like: 25, ment: 2,imgUrl: "https://i.namu.wiki/i/9aUQQ4YjU9vmKuHT_cZAL61VKpKsLolynnI46BhOZQuKxGJygZ6BJK2zTHoX3pcNQmmcfzcVEZQcythY1lRXBQ.webp" },
+        { id: 11, title: "친구한테 질투하는 내가 싫어요", author: "익명", date: "2025-05-11", views: 30 ,like: 25, ment: 2},
+        { id: 10, title: "누군가 계속 쳐다보는 것 같아요", author: "익명", date: "2025-05-10", views: 26,like: 25, ment: 2, imgUrl: "https://i.namu.wiki/i/Va3Dy_3qFHvGQS4qwv0oCvFySbT1DXJkK0zfMosd2UK6Jun8Zucb796VLJzLL4A40e5P4dgbBPT4da2Bv_S50Q.webp" },
+        { id: 9, title: "그냥 사라지고 싶다는 생각이 자주 들어요", author: "익명", date: "2025-05-09", views: 47 ,like: 25, ment: 2},
+        { id: 8, title: "미래가 너무 막막해서 무서워요", author: "익명", date: "2025-05-08", views: 33,like: 25, ment: 2, imgUrl: "https://mblogthumb-phinf.pstatic.net/MjAyMjExMTZfMTQz/MDAxNjY4NjAxNTQzNDYw.dZzc1TIxWmITM_5uildryLFGXFwgjx0ahbrf9DXCEZ0g.xTwwfY8Je-4zuVM3FbIm2WDtEY0b9YSimgX4RM6MCsEg.JPEG.gngnt2002/%EF%BB%BF%EC%9A%B0%EB%8A%94_%EA%B3%A0%EC%96%91%EC%9D%B4_%EC%A7%A4_%EB%AA%A8%EC%9D%8C_(4).jpg?type=w800" },
+        { id: 7, title: "누군가에게 의미 있는 사람이 되고 싶어요", author: "익명", date: "2025-05-07", views: 28 ,like: 25, ment: 2},
+        { id: 6, title: "SNS 속 나는 가짜 같아요", author: "익명", date: "2025-05-06", views: 39 ,like: 25, ment: 2},
+        { id: 5, title: "자존감이 점점 낮아져요", author: "익명", date: "2025-05-05", views: 32, like: 25, ment: 2, imgUrl: "https://i.kym-cdn.com/photos/images/original/001/389/404/a2b.jpg" },
+        { id: 4, title: "밤마다 걱정이 많아져서 잠이 안 와요", author: "익명", date: "2025-05-04", views: 36 ,like: 25, ment: 2},
+        { id: 3, title: "무리 속에서도 혼자인 느낌 들어요", author: "익명", date: "2025-05-03", views: 19 ,like: 25, ment: 2},
+        { id: 2, title: "나도 남들처럼 살고 싶어요", author: "익명", date: "2025-05-02", views: 25 ,like: 25, ment: 2},
+        { id: 1, title: "익명이라 말해요, 저 지금 우울해요", author: "익명", date: "2025-05-01", views: 51, like: 25, ment: 2, imgUrl: "https://img.animalplanet.co.kr/news/2023/01/22/700/q7o0b0yt4vf84289yw88.jpg" }
       ];
-      
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 8;
 
@@ -39,29 +38,31 @@ const SecretBoardBox = () => {
   return (
     <div className="SecretBoardBox">
       <div className="secret-header">
-        <h2 className="secret-title">비밀게시판</h2>
-        {/* <Link to="/main/secret/write" className="write-button">글쓰기</Link> */}
+        <h2 className="Secret-title">비밀게시판</h2>
       </div>
 
       <div className="secret-list">
         {visibleSecrets.map((secret) => (
           <div key={secret.id} className="secret-list-item">
             {secret.imgUrl && (
-              <img
-                src={secret.imgUrl}
-                alt="썸네일"
-                className="secret-thumbnail"
-              />
+              <img src={secret.imgUrl} alt="썸네일" className="secret-thumbnail" />
             )}
             <div className="secret-list-content">
-              <Link to={`/main/secret/${secret.id}`} className="secret-title-link">
-                {secret.title}
-                {secret.views >= 40 && (
-                  <span className="hot-badge">인기</span>
-                )}
+              <Link to={`/main/secret/${secret.id}`} className="secret-link">
+                <div className="secret-title-line">
+                  <div className="secret-title-wrapper">
+                    <h3 className="secret-title">{secret.title}</h3>
+                    {secret.views >= 40 && (
+                      <span className="hot-badge">인기</span>
+                    )}
+                  </div>
+                  <div className="secret-author-date">
+                    {secret.author} | {secret.date}
+                  </div>
+                </div>
               </Link>
-              <div className="secret-meta">
-                {secret.author} | {secret.date} | 조회 {secret.views}
+              <div className="secret-meta-line">
+                조회 {secret.views} | ❤️ {secret.like} | 댓글 {secret.ment}
               </div>
             </div>
           </div>
@@ -70,9 +71,7 @@ const SecretBoardBox = () => {
 
       <div className="pagination">
         {currentPage > 1 && (
-          <button onClick={() => handlePageChange(currentPage - 1)}>
-            &lt; 이전
-          </button>
+          <button onClick={() => handlePageChange(currentPage - 1)}>&lt; 이전</button>
         )}
         {Array.from({ length: totalPages }, (_, index) => (
           <button
@@ -84,9 +83,7 @@ const SecretBoardBox = () => {
           </button>
         ))}
         {currentPage < totalPages && (
-          <button onClick={() => handlePageChange(currentPage + 1)}>
-            다음 &gt;
-          </button>
+          <button onClick={() => handlePageChange(currentPage + 1)}>다음 &gt;</button>
         )}
       </div>
     </div>
