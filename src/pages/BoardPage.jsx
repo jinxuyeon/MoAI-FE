0ebfbe3d8cdae9cc45a877d1efe1cc30aa831dc2
yevinSeo <145316741+yevinSeo@users.noreply.github.com>
2025-05-27 16:27:29 +0900

    ㅇ
    ㅇ

--- src/pages/BoardPage.jsx
+++ src/pages/BoardPage.jsx
@@ -47,10 +47,7 @@ const BoardPage = () => {
                     <div className="navibar-container">
                         <NaviBar currentBoard={boardType} />
                         {/* boardType이 "lecture"인 경우 글쓰기 버튼 숨김 */}
-                        {boardType !== "lecture" && (
-                            <button className="write-button" onClick={handleWriteClick}>
-                                글쓰기
-                            </button>
+                    
                         {boardType === "lecture" ? (
                             <button className="write-button" onClick={() => navigate("/create-lecture")}>
                                 생성
