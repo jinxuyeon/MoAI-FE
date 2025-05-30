2554ee503563ced46a4611e8004cbba1401118e2
choiyoonjoo <145316737+choiyoonjoo@users.noreply.github.com>
2025-05-30 17:32:16 +0900

    게시글

--- src/App.jsx
+++ src/App.jsx
@@ -76,6 +76,8 @@ function App() {
                     <Route path="/write/:boardType" element={<WritePage />} />
                     <Route path="/main/lecture/:lectureId/write" element={<LectureWritePage />} />
                     <Route path="/admin" element={<AdminPage />} />
+                    <Route path="/main/community/:boardType/post/:postId" element={<BoardPage />} />
+
 
 
 
