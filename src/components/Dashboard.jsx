078ee892ce7486ba640a78805e84d6dfac6c8d79
SonJHo <wnsgh708222@naver.com>
2025-06-03 22:03:27 +0900

    대시보드 레이아웃 개선선

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -13,34 +13,37 @@ const Dashboard = () => {
                         <House size={32} />
                         <h1>Home</h1>
                     </div>
-
+                    <div className="title-container">
+                        <Volume2 size={20} color="var(--normal-text-color)" />
+                        <h3>From the Office</h3>
+                    </div>
                     <section className="inner-container">
-                        <div className="title-container">
-                            <Volume2 size={20} color="var(--normal-text-color)" />
-                            <h3>From the Office</h3>
-                        </div>
+
                         <BasicBoard type={"NOTICE_C"} title={"학과사무실에서 알려드립니다"} />
                     </section>
-                    <section className="inner-container">
+                    <div>
                         <h3>Join the Conversation</h3>
-                        <section className="div-container">
-                            <div className="half-board-area">
-                                <BasicBoard type={"FREE"} title={"자유게시판"} />
-                            </div>
-                            <div className="half-board-area">
-                                <BasicBoard type={"SECRET"} title={"비밀게시판"} />
-                            </div>
-                        </section>
+                        <section className="inner-container">
+                            <section className="div-container">
+                                <div className="half-board-area">
+                                    <BasicBoard type={"FREE"} title={"자유게시판"} />
+                                </div>
+                                <div className="half-board-area">
+                                    <BasicBoard type={"SECRET"} title={"비밀게시판"} />
+                                </div>
+                            </section>
 
-                        <section className="div-container">
-                            <div className="half-board-area">
-                                <BasicBoard type={"FREE"} title={"선배님 고민있어으예 ~"} />
-                            </div>
-                            <div className="half-board-area">
-                                <BasicBoard type={"FREE"} title={"취업, 면접 후기"} />
-                            </div>
+                            <section className="div-container">
+                                <div className="half-board-area">
+                                    <BasicBoard type={"FREE"} title={"선배님 고민있어으예 ~"} />
+                                </div>
+                                <div className="half-board-area">
+                                    <BasicBoard type={"FREE"} title={"취업, 면접 후기"} />
+                                </div>
+                            </section>
                         </section>
-                    </section>
+                    </div>
+
                 </div>
 
                 <div className="rightside-container">
