d98078f6836dca0a78a5e40bcc19dccdbb840376
sy <ssyeo_n@naver.com>
2025-06-16 20:59:17 +0900

    chore: 환경 변수 설정 - .env 파일 생성 (서버 URL 선언) - gitignore에 .env 추가 - vite.config.js에 프록시 설정 추가 - Axios 인스턴스 수정 (baseURL을 환경 변수 파일에서 import) - ESLint 설정 업데이트 (Node.js 전역 변수 인식 못하는 문제 해결) - 패키지 의존성 추가 및 버전 업데이트 (dotenv 등)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from "dotenv";
+
+// .env 파일 로드
+dotenv.config();
+
+const baseURL = process.env.VITE_API_BASE_URL
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // 개발 서버 설정
+    server: {
+        proxy: {
+            // CORS 문제 해결을 위한 프록시 설정
+            "/api": {
+                target: baseURL,
+                changeOrigin: true, // 요청 헤더의 origin을 target URL로 변경
+                secure: false, // HTTP도 허용 (배포 환경에서는 true로 설정하여 HTTPS 사용)
+                credentials: true, // 쿠키 등 인증 정보 포함한 요청 허용
+            },
+        },
+    },
 })
