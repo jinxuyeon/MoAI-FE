cde74a2e963bec115e9076fecb1d23b6e24605d3
SonJHo <wnsgh708222@naver.com>
2025-07-13 21:27:56 +0900

    feat: InfoBox 게시판으로 이동 버튼 구현

--- src/components/utils/boardUtils.jsx
+++ src/components/utils/boardUtils.jsx
@@ -1,13 +1,16 @@
 
 // 여기만고치면돼 게시판 개설
-const boardTypeMap = {
-    NOTICE_C: "학과 공지사항",
-    NOTICE: "조교 전달사항",
+const boardTypeMap = { //게시판 페이지 네이게이션 바 용
+    NOTICE: "조교알림",
+    NOTICE_C: "공지사항",
     FREE: "자유게시판",
     SECRET: "비밀게시판",
-    REVIEW: "취업,면접 후기",
+    REVIEW: "후기",
     MARKET: "책장터",
 };
+
+
+
 
 
 export const getBoardLabel = (type = "") => {
