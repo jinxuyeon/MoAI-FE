76989e69d1eff048e39772099dbdd4404f81fc64
sy <ssyeo_n@naver.com>
2025-07-14 21:53:02 +0900

    design: 로그인, 회원가입 페이지 반응형 디자인 적용 - App.css에서 폰트 사이즈 변수를 추가하고 반응형 디자인을 위한 미디어 쿼리 설정 - LoginForm.css와 RegisterForm.css에서 폰트 사이즈 변수를 적용하여 일관성 유지 - IntroBox.jsx에서 텍스트 수정 및 CSS 클래스 정리 - vite.config.js에서 서버 호스트 설정 추가

--- src/components/IntroBox.jsx
+++ src/components/IntroBox.jsx
@@ -4,7 +4,7 @@ const IntroBox = () => {
         <section className="intro-container">
             <h1 className="intro">
                 지금 로그인하고
-                <br /> 인공지능 공학부 커뮤니티에
+                <br /> 인공지능공학부 커뮤니티에
                 <br /> 참여하세요.
             </h1>
 
