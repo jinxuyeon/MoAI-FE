d0d3740c0aadb372919db012611f8c3478b7ec55
SonJHo <wnsgh708222@naver.com>
2025-07-28 21:39:35 +0900

    design:카드 디자인 수정

--- src/components/LectureCard.jsx
+++ src/components/LectureCard.jsx
@@ -7,13 +7,20 @@ const LectureCard = ({ lecture }) => {
       to={`/main/study-dashboard/${lecture.id}`}
       className="lecture-card"
       state={{ lecture }}
-      title={lecture.intro || ""}
       style={{ borderLeft: `5px solid ${lecture.themeColor}` }}
     >
       <div className="lecture-card-main">
-        <h3>{lecture.title}</h3>
-        <p>{lecture.professorName} 교수님</p>
-        <p>{lecture.grade}학년 {lecture.semester}학기</p>
+        <h3 className="lecture-title">{lecture.title}</h3>
+        
+        <p className="lecture-meta">
+          {lecture.grade}학년 {lecture.semester}학기
+        </p>
+        <p className="lecture-professor">
+          {lecture.professorName} 교수님
+        </p>
+        <p className="lecture-intro">
+          {lecture.intro ? lecture.intro : "강의 소개가 없습니다."}
+        </p>
       </div>
 
       <div className="marked-count">
