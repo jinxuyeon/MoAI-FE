360c6aae6345497fb824b28a0699c813452330cb
SonJHo <wnsgh708222@naver.com>
2025-09-16 16:29:27 +0900

    굿

--- src/components/IntroBox.jsx
+++ src/components/IntroBox.jsx
@@ -4,6 +4,7 @@ const IntroBox = () => {
         <section className="intro-container">
             <h1 className="intro">
                 지금 로그인하고
+                <br /> 한국해양대학교
                 <br /> 인공지능공학부 커뮤니티에
                 <br /> 참여하세요.
             </h1>
